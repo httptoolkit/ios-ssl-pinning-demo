d585f9e791cd5f8016902f98fd79a58ac3a26ad7
Tim Perry <pimterry@gmail.com>
2023-12-19 08:51:04 -0800

    Initial XCode app setup

---
+++ ios-pinning-demo/ios_pinning_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_pinning_demoApp.swift
+//  ios-pinning-demo
+//
+//  Created by Tim Perry on 19/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ios_pinning_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
